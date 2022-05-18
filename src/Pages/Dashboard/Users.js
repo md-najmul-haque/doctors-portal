049e8748ffff89f36b73d3ea063b0365d8bd7139
najuml-bd <najmul0612@gmail.com>
2022-05-18 21:39:08 +0600

    make admin from user option added

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -5,7 +5,7 @@ import UserRow from './UserRow';
 
 const Users = () => {
 
-    const { data: users, isLoading } = useQuery('users', () => fetch(`http://localhost:5000/users`, {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch(`http://localhost:5000/users`, {
         method: "GET",
         headers: { authorization: `Bearer, ${localStorage.getItem('accessToken')}` }
 
@@ -33,7 +33,7 @@ const Users = () => {
                             users.map(user => <UserRow
                                 key={user._id}
                                 user={user}
-
+                                refetch={refetch}
                             ></UserRow>)
                         }
                     </tbody>
