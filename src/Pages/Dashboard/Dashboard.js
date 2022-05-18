5ec67aea47b864d69112dd7ebcb540efb6168b7a
najuml-bd <najmul0612@gmail.com>
2022-05-19 00:05:07 +0600

    useAdmin hook created to sucure make admin section

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
-
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
 
     return (
         <div>
@@ -17,7 +21,7 @@ const Dashboard = () => {
                     <ul className="menu p-4 overflow-y-auto w-84 bg-base-100 text-base-content">
                         <li><Link to='/dashboard'>My Appointments</Link></li>
                         <li><Link to='/dashboard/review'> My Reviews</Link></li>
-                        <li><Link to='/dashboard/user'>All Users</Link></li>
+                        {admin && <li><Link to='/dashboard/user'>All Users</Link></li>}
 
                     </ul>
 
