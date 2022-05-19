a8ddd3396bb27bf4d485cee27d60829e14edf541
najuml-bd <najmul0612@gmail.com>
2022-05-19 18:38:36 +0600

    add doctor conpleted

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 import Loading from '../Shared/Loading';
 
 const AddDoctor = () => {
 
-    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
 
     const { data: services, isLoading } = useQuery('services', () => fetch(`http://localhost:5000/services`).then(res => res.json()))
+
+    const imageStorageKey = 'd90f0f7587defd256d8cd2cd85b0500d'
 
     /**
  * 3 ways to store images
@@ -19,7 +22,51 @@ const AddDoctor = () => {
 */
 
     const onSubmit = async data => {
-        console.log(data)
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+
+
+        const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`
+
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.success) {
+                    const img = result.data.url;
+                    const doctor = {
+                        name: data.name,
+                        email: data.email,
+                        specialty: data.specialty,
+                        image: img
+
+                    }
+                    // send to data base,
+                    fetch(`http://localhost:5000/doctor`, {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                        },
+                        body: JSON.stringify(doctor)
+
+                    })
+                        .then(res => res.json())
+                        .then(inserted => {
+                            if (inserted.insertedId) {
+                                toast.success('Doctor added successfully');
+                                reset();
+                            } else {
+                                toast.error('Failed to add the doctor')
+                            }
+
+                        })
+                }
+
+            })
     }
 
     if (isLoading) {
